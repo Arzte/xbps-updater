604141471caec167b8d72146e0ad5502dcbf68f5
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2018-01-15 16:20:05 -0500

    Make main only be a runner for the run() function, so I can ~~abuse~~Ahem, use ? for Results

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,19 @@ extern crate failure_derive;
 extern crate serde_derive;
 
 mod github;
+mod update;
 
 use failure::Error;
+use update::check_for_updates;
 use github::user::get_user;
 
 fn main() {
+     if let Err(err) = run() {
+        panic!("xbps-updater encountered an unrecoverable error:\n\n\t{}\n", err);
+    }
+}
+
+fn run() -> Result<(), Box<Error>> {
     println!(
         "{}",
         get_user("Vaelatern@gmail.com").unwrap_or("Error getting email.".to_owned())
