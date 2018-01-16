290130b782841f001f8bd2a191578bc5471678ae
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2018-01-15 22:20:22 -0500

    Pharse ./xbps-src output rather than just returning it verbatium. This has the function return latest version if xbps-src finds one, or returns empty if non where found (possibly error to but outcome is mostly the same, no?)

--- src/update.rs
+++ src/update.rs
@@ -5,5 +5,14 @@ pub fn check_for_updates(pkg: &str) -> Result<String, Error> {
             .arg("update-check")
             .arg(pkg)
             .output()?;
-    Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    let output_stderr = String::from_utf8_lossy(&output.stderr).to_string();
+    let output_stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    if output_stderr.is_empty() {
+        return Ok(output_stdout.split(" ").last().unwrap().split("-").last().unwrap().to_owned());
+    // TODO: Properly check output to see if no update was needed/Or no update was found/Or other error
+    } else if output_stdout.is_empty() {
+        return Ok("".to_owned())
+    } else {
+        bail!("Unknown ./xbps-src output".to_owned());
+    }
 }
