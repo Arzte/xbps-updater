88b61c773189c63ec13ef5fea14e6bfc889ed593
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2018-01-24 00:01:22 -0500

    #[skip_ci] Workspace push so laptop can work on latest version.

--- src/update.rs
+++ src/update.rs
@@ -9,6 +9,13 @@ pub enum xbps_src_error {
     #[fail(display = "Unexpected xbps-src output: {}", _0)]
     unknown_error(String),
 }
+
+#[derive(Fail, Debug)]
+pub enum xbps_updater_error {
+    #[fail(display = "xbps-src error")]
+    xbps_src_error,
+}
+
 pub fn check_update(pkg: &str) -> Result<String, xbps_src_error> {
     let output = ::std::process::Command::new("/home/doctors/git/void-packages/xbps-src")
             .arg("update-check")
@@ -25,10 +32,24 @@ pub fn check_update(pkg: &str) -> Result<String, xbps_src_error> {
             "NO VERSION found for" => Err(xbps_src_error::no_version(output_stderr)),
             "" => Err(xbps_src_error::latest_version),
             _ => Err(xbps_src_error::unknown_error(output_stderr)),
+        }
     }
 }
-}
 
-pub update_package(pkg: &str) -> Result<String, Error> {
-    check_update(pkg);
+pub fn update_package(pkg: &str) -> Result<String, Error> {
+    let update = check_update(pkg);
+    if update.is_ok() {
+        let output = ::std::process::Command::new("xgensum")
+            .arg(format!("/home/doctors/git/void-packages/srcpkgs/{}", pkg))
+            .output()
+            .unwrap();
+        let output_stderr = String::from_utf8_lossy(&output.stderr).to_string();
+        Ok(output_stderr)
+    } else {
+        match update.err() {
+            xbps_src_error::no_version => xbps_src_error::no_version,
+            xbps_src_error::latest_version => xbps_src_error::latest_version,
+            xbps_src_error::unknown_error => xbps_src_error::unknown_error,
+        }
+    }
 }
